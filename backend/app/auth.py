e9c7423d1369e2bb22cd749cfb9890b837bba564
venu <ganduvenureddy@gmail.com>
2026-03-09 21:26:04 +0530

    update - Job Scraper Platform

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "yoursecretkey")
-ALGORITHM = os.getenv("ALGORITHM", "HS256")
+SECRET_KEY = os.getenv("JWT_SECRET")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 def verify_password(plain_password, hashed_password):
